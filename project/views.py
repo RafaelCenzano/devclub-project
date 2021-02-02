1fee0fb5b4a1bca4127988540e1ca6f7a3f64327
Rafael Cenzano <savagecoder77@gmail.com>
2021-02-02 22:44:40 +0000

    Update templates

--- project/views.py
+++ project/views.py
@@ -4,3 +4,12 @@ from flask import render_template, url_for
 @app.route('/')
 def home():
   return render_template('home.html')
+
+@app.route('/hello/<name>')
+def hello(name):
+  return render_template('hello.html', name=name)
+
+@app.route('/list')
+def listThings():
+  names = ['Elon Musk', 'Bill Gates', 'Mark Zuckerberg']
+  return render_template('names.html', names=names)
